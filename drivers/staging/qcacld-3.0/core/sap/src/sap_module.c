df63a977b6aa2ca9400ef5c815059772ab67d630
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-05 14:54:15 -0700

    qcacld-3.0: Nuke as much debug bloat as possible
    The overhead from all the debugging in this monstrosity of a driver is
    measurably significant. Chop it all out.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [kdrag0n: adapted for Pixel 3 Wi-Fi stack]
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    
    Adapted for qcacld 5.2.03
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: SGCMarkus <markusornik@gmail.com>

--- drivers/staging/qcacld-3.0/core/sap/src/sap_module.c
+++ drivers/staging/qcacld-3.0/core/sap/src/sap_module.c
@@ -1252,38 +1252,6 @@ wlansap_update_csa_channel_params(struct sap_context *sap_context,
 }
 
 /**
- * sap_get_csa_reason_str() - Get csa reason in string
- * @reason: sap reason enum value
- *
- * Return: string reason
- */
-static char *sap_get_csa_reason_str(enum sap_csa_reason_code reason)
-{
-	switch (reason) {
-	case CSA_REASON_UNKNOWN:
-		return "UNKNOWN";
-	case CSA_REASON_STA_CONNECT_DFS_TO_NON_DFS:
-		return "STA_CONNECT_DFS_TO_NON_DFS";
-	case CSA_REASON_USER_INITIATED:
-		return "USER_INITIATED";
-	case CSA_REASON_PEER_ACTION_FRAME:
-		return "PEER_ACTION_FRAME";
-	case CSA_REASON_PRE_CAC_SUCCESS:
-		return "PRE_CAC_SUCCESS";
-	case CSA_REASON_CONCURRENT_STA_CHANGED_CHANNEL:
-		return "CONCURRENT_STA_CHANGED_CHANNEL";
-	case CSA_REASON_UNSAFE_CHANNEL:
-		return "UNSAFE_CHANNEL";
-	case CSA_REASON_LTE_COEX:
-		return "LTE_COEX";
-	case CSA_REASON_CONCURRENT_NAN_EVENT:
-		return "CONCURRENT_NAN_EVENT";
-	default:
-		return "UNKNOWN";
-	}
-}
-
-/**
  * wlansap_set_channel_change_with_csa() - Set channel change with CSA
  * @sapContext: Pointer to SAP context
  * @targetChannel: Target channel
@@ -1330,13 +1298,6 @@ QDF_STATUS wlansap_set_channel_change_with_csa(struct sap_context *sapContext,
 			"%u is unsafe channel", targetChannel);
 		return QDF_STATUS_E_FAULT;
 	}
-	QDF_TRACE(QDF_MODULE_ID_SAP, QDF_TRACE_LEVEL_INFO,
-		"%s: sap chan:%d target:%d conn on 5GHz:%d, csa_reason:%s(%d)",
-		__func__, sapContext->channel, targetChannel,
-		policy_mgr_is_any_mode_active_on_band_along_with_session(
-			pMac->psoc, sapContext->sessionId, POLICY_MGR_BAND_5),
-			sap_get_csa_reason_str(sapContext->csa_reason),
-			sapContext->csa_reason);
 
 	sta_sap_scc_on_dfs_chan =
 		policy_mgr_is_sta_sap_scc_allowed_on_dfs_chan(pMac->psoc);
